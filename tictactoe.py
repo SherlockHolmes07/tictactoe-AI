e15f4073e722768dd96f81baf4b3492ac7426b76
Dhairya Upadhyay <dhairya221b@gmail.com>
2022-09-21 00:29:39 +0530

    implemented winner

--- tictactoe.py
+++ tictactoe.py
@@ -74,7 +74,22 @@ def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    
+    for i in range(len(board)):
+        if board[i][1] == board[i][0] and board[i][1] == board[i][2]:
+            return board[i][1]
+    
+    for i in range(len(board[1])):
+        if board[1][i] == board[0][i] and board[1][i] == board[2][i]:
+            return board[1][i]
+
+    if board[1][1] == board[0][0] and board[1][1] == board[2][2]:
+        return board[1][1]
+
+    if board[1][1] == board[2][0] and board[1][1] == board[0][2]:
+        return board[1][1]
+
+    return None  
 
 
 def terminal(board):
